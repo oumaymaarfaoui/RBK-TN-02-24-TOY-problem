64fc6dfdea02dbda1be871c5e643e35ba8cf5cb3
oumaima arfaoui <oumaymaarfaoui>
2024-06-06 09:46:17 +0100

    toy3

--- commonCharacters.js
+++ commonCharacters.js
@@ -41,3 +41,30 @@ var commonCharacters=function(string1, string2) {
     return common
     
 }
+
+
+function findcharacter(string1, string2) {
+    var res=[]
+    var res={}
+    for(var i=0; i<string1.length; i++) {
+        var char=string1[i]
+        if(char !=="" && string2.includes(char)&& !exist[char]) {
+            exist[char]=true
+            res.push(char)
+        }
+    }
+    return res.join("")
+}
+
+
+function findCaracter(string1,string2) {
+    var res=""
+    for(var i=0; i<string1.length; i++) {
+        for(var j=0; j<string2.length; j++) {
+            if( string1[i]===string2[j] && !res.includes(string1[i]) && string1[i]!=="") {
+                res+=string1[i]
+            }
+        }
+    }
+    return res || null
+}
